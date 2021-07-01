89f30122a3f01337244eb5f9e53e328ea4fc505e
wpatty <wpatty@wavious.com>
2021-07-01 08:56:16 -0700

    Wavious Build Infrastructure
    Fixes:
      - None
    
    Features:
      - CMake Helper Macros for extracting Semantic Versioning and
        Git properties
      - Python Script to patch ELF image with correct image size
        and CRC values
      - Image Header definiton for use in Wavious Software Projects

---
+++ cmake/FindBuildInfo.cmake
@@ -0,0 +1,34 @@
+# Generate Metadata from Git
+find_package(GetVersionFromGitTag)
+
+# Build Timestamp
+string(TIMESTAMP BUILD_DATE "%d-%m-%Y, %H:%M" UTC )
+
+# Build Machine
+cmake_host_system_information(RESULT BUILD_MACHINE QUERY HOSTNAME)
+
+# Ensure Python is installed
+find_package(Python REQUIRED)
+
+# Define the python binary
+set(IMAGE_PATCH_SCRIPT_PATH ${WAV_BUILD_TOP_LEVEL}/scripts/patch_image_header.py)
+
+# Create a macro that can patch image after its built
+macro(patch_image_header target prefix)
+    add_custom_command(TARGET ${target}
+        POST_BUILD
+        COMMAND ${Python_EXECUTABLE} ${IMAGE_PATCH_SCRIPT_PATH} ${target} --prefix ${prefix}
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+    )
+endmacro()
+
+# Build Info
+message("BUILD DATE:    ${BUILD_DATE}")
+message("BUILD MACHINE: ${BUILD_MACHINE}")
+message("BUILD MAJOR:   ${VERSION_MAJOR}")
+message("BUILD MINOR:   ${VERSION_MINOR}")
+message("BUILD PATCH:   ${VERSION_PATCH}")
+message("BUILD TWEAK:   ${VERSION_TWEAK}")
+message("BUILD AHEAD:   ${VERSION_AHEAD}")
+message("BUILD SHA:     ${VERSION_GIT_SHA}")
+message("BUILD DIRTY:   ${VERSION_DIRTY}")
